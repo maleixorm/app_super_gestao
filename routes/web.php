7f33d4ccf20e4887dee84bc8cebcb412c52ae376
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-06 13:32:11 -0300

    nomeando as rotas da aplicação

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/login', function() { return 'Login'; });
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre'])->name('site.sobre');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function() { return 'Clientes'; });
-    Route::get('/fornecedores', function() { return 'Fornecedores'; });
-    Route::get('/produtos', function() { return 'Produtos'; });
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
 });
