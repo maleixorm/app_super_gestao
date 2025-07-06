505ebadd4b6e34ef7b26eeca9e7aa19239292b66
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-06 13:23:09 -0300

    Agrupando rotas através de um prefixo

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre']);
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
 Route::get('/login', function() { return 'Login'; });
-Route::get('/clientes', function() { return 'Clientes'; });
-Route::get('/fornecedores', function() { return 'Fornecedores'; });
-Route::get('/produtos', function() { return 'Produtos'; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; });
+    Route::get('/fornecedores', function() { return 'Fornecedores'; });
+    Route::get('/produtos', function() { return 'Produtos'; });
+});
