d8e6d6f8d27077fea5f219c10fd7a5bc8fbe1637
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-05 21:05:36 -0300

    Trabalhando com envio de parâmetros nas rotas

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) 
+    {
+        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+    }
+);
