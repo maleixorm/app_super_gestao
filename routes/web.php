cf444eee07ba421c8c3d6c4720dca8ffb3f76871
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-06 13:14:09 -0300

    Mantendo as rotas que serão utilizadas no projeto

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,3 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-
-Route::get('/contato/{nome}/{categoria_id}', function(string $nome = 'Desconhecido', int $categoria_id = 1) 
-    {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
