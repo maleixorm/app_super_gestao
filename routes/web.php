fe5a779a8e97a6106e3a8f1c9f3408393d5fa03d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-07 16:37:31 -0300

    alterando as rotas

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
-Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre'])->name('site.sobre');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function() { return 'Login'; })->name('site.login');
+Route::get('/sobre', [\App\Http\Controllers\SobreController::class, 'sobre'])->name('site.sobre');
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('site.teste');
 
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
@@ -13,7 +14,6 @@ Route::prefix('/app')->group(function() {
     Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
 });
 
-Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
 
 Route::fallback(function() {
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para voltar à pagina inicial!';
