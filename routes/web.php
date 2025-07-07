abd4cdbefcb0aeb362f822d2d3650f54062046be
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-07 10:25:18 -0300

    Criando a rota para o controlador de teste

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,7 @@ Route::prefix('/app')->group(function() {
     Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
 });
 
-Route::get('/rota1', function() {
-    echo 'Rota 1';
-})->name('site.rota1');
-
-Route::get('/rota2', function() {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-// Route::redirect('/rota2', './rota1');
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
 
 Route::fallback(function() {
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para voltar à pagina inicial!';
