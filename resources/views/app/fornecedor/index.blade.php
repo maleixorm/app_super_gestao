dacf81f3678f883973edf9038b52610fcd076af4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-08 00:00:22 -0300

    Usando o @unless para uma condicional negativa

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -9,3 +9,7 @@
 Fornecedor: {{ $fornecedores[0]['nome'] }}
 <br>
 Status: {{ $fornecedores[0]['status'] }}
+<br>
+@unless($fornecedores[0]['status'] == 'S')
+    <h5>Fornecedor inativo</h5>
+@endunless
