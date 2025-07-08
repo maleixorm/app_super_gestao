cb8f0751d0085fd27b71d946c77b3b50cc4ec14f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-07 23:46:57 -0300

    criando um array e passando por parâmetro para a view

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,6 +8,19 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1',
+            'Fornecedor 1'
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
