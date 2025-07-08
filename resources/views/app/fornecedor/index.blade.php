0681b446f4ecc3f6c61e9c60b1bfad109de86e28
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-07 23:53:52 -0300

    verificando o que foi recebido pela view

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,10 +6,4 @@
 @endphp
 
 
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h4>Existem alguns fornecedores cadastrados</h4>
-@elseif(count($fornecedores) > 10)
-    <h4>Existem vários fornecedores cadastrados</h4>
-@else
-    <h4>Ainda não existem fornecedores cadastrados</h4>
-@endif
+@dd($fornecedores)
