9d3c24dddaa3aabd2dfaacdb1fda8268f7445695
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-11 23:24:32 -0300

    adicionando dados aos fornecedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,9 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status'=> 'N'],
-            1 => ['nome' => 'Fornecedor 2', 'status'=> 'S', 'cnpj' => '00.000.000/0000-00'],
+            0 => ['nome' => 'Fornecedor 1', 'status'=> 'N', 'cnpj' => '12.234.345/6789-01', 'ddd' => '11', 'telefone' => '99999-8888'],
+            1 => ['nome' => 'Fornecedor 2', 'status'=> 'S', 'cnpj' => '00.000.000/0000-00', 'ddd' => '85', 'telefone' => '98888-7777'],
+            2 => ['nome' => 'Fornecedor 3', 'status'=> 'S', 'cnpj' => '12.345.678/0001-90', 'ddd' => '32', 'telefone' => '97777-6666'],
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
