35248d8f71c4733d35f04d3c9769223a0f2fac4b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-11 23:24:14 -0300

    Implementando o switch case no blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,9 +6,26 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    Fornecedor: {{ $fornecedores[2]['nome'] }}
     <br>
-    Status: {{ $fornecedores[1]['status'] }}
+    Status: {{ $fornecedores[2]['status'] }}
     <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não informado' }}
+    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não informado' }}
+    <br>
+    Telefone: {{ $fornecedores[2]['ddd'] ?? 'Dado não informado' }} - {{ $fornecedores[2]['telefone'] ?? 'Dado não informado' }}
+    <br>
+    Cidade: 
+    @switch($fornecedores[2]['ddd'])
+        @case ('11')
+            São Paulo - SP
+            @break
+        @case ('32')
+            Juiz de Fora - MG
+            @break
+        @case ('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não informado
+    @endswitch
 @endisset
