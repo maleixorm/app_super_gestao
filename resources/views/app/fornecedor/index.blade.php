0bbd3de27bed6748a4da5b5eb45b5b38f13e6895
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-28 16:37:57 -0300

    Utilizando o For no Blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,26 +6,14 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[2]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[2]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não informado' }}
-    <br>
-    Telefone: {{ $fornecedores[2]['ddd'] ?? 'Dado não informado' }} - {{ $fornecedores[2]['telefone'] ?? 'Dado não informado' }}
-    <br>
-    Cidade: 
-    @switch($fornecedores[2]['ddd'])
-        @case ('11')
-            São Paulo - SP
-            @break
-        @case ('32')
-            Juiz de Fora - MG
-            @break
-        @case ('85')
-            Fortaleza - CE
-            @break
-        @default
-            Estado não informado
-    @endswitch
+    @for ($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não informado' }}
+        <br>
+        Telefone: {{ $fornecedores[$i]['ddd'] ?? 'Dado não informado' }} - {{ $fornecedores[$i]['telefone'] ?? 'Dado não informado' }}
+        <hr>
+    @endfor 
 @endisset
