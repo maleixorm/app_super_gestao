b03aaa5b43a1ed96fb0ee52778110f9d155854ad
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-29 14:54:07 -0300

    Criando um template basico para as views

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
+    </head>
+    <body>
+        @yield('conteudo')
+    </body>
+</html>
