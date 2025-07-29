d548c7f6a746f73302462aef39c67201e68288d9
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-29 15:04:37 -0300

    Gerando o titulo de forma dinâmica nas views

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Super Gestão - @yield('titulo')</title>
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     </head>
     <body>
