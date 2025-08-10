610cd993a60b5d18321ed52cf1b7c6b8153853cb
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-10 11:22:59 -0300

    Implementando o método down da migration alter fornecedores

--- database/migrations/2025_08_10_014050_alter_fornecedores_novas_colunas.php
+++ database/migrations/2025_08_10_014050_alter_fornecedores_novas_colunas.php
@@ -24,5 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn(['uf', 'email']);
+        });
     }
 };
