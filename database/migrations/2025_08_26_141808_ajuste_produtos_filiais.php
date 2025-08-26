1cf08563876a52756d64f121fc525b0361123781
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-26 11:42:30 -0300

    Adicionando as foreign keys à tabela produto filiais

--- database/migrations/2025_08_26_141808_ajuste_produtos_filiais.php
+++ database/migrations/2025_08_26_141808_ajuste_produtos_filiais.php
@@ -27,6 +27,10 @@ return new class extends Migration
             $table->integer('estoque_minimo');
             $table->integer('estoque_maximo');
             $table->timestamps();
+
+            // foreign keys (constraints)
+            $table->foreign('filial_id')->references('id')->on('filiais');
+            $table->foreign('produto_id')->references('id')->on('produtos');
         });
         
         // removendo colunas da tabela produtos
